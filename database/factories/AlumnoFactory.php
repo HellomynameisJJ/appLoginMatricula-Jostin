0d34ba7beb6d3e33388d6bb2e45a61e0e6d4e329
Walter Guevara <wd123456689@icloud.com>
2026-05-29 08:00:46 -0500

    He creado las tablas para la bd, falta introducir los atributos que ahora lo hago, verificar

---
+++ database/factories/AlumnoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\alumno;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<alumno>
+ */
+class AlumnoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
