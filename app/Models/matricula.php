3eb3ac004146f6486b40ccb4f3ad6661a5c52680
jean .P <Cayth78@gmail.com>
2026-05-29 09:29:53 -0500

    agregue las relaciones  de matricula en su migracion y en su modelo

--- app/Models/matricula.php
+++ app/Models/matricula.php
@@ -9,4 +9,35 @@ class matricula extends Model
 {
     /** @use HasFactory<\Database\Factories\MatriculaFactory> */
     use HasFactory;
+    protected $primaryKey = 'id_matricula';
+    protected $fillable = [
+        'id_alumno',
+        'id_curso',
+        'id_profesor',
+        'id_horario',
+        'semestre',
+        'fecha_matricula',
+        'nota_final',
+        'estado',
+    ];
+
+    public function alumno()
+    {
+        return $this->belongsTo(Alumno::class, 'id_alumno');
+    }
+
+    public function curso()
+    {
+        return $this->belongsTo(Curso::class, 'id_curso');
+    }
+
+    public function profesor()
+    {
+        return $this->belongsTo(Profesor::class, 'id_profesor');
+    }
+
+    public function horario()
+    {
+        return $this->belongsTo(Horario::class, 'id_horario');
+    }
 }
--- database/migrations/2026_05_29_125746_create_matriculas_table.php
+++ database/migrations/2026_05_29_125746_create_matriculas_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('matriculas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('alumno_id');
+            $table->foreign('id_alumno')->references ('id')->on('alumnos'->onDelete('cascade');
+            $table->unsignedBigInteger('id_curso');
+            $table->foreign('id_curso')->references('id')->on('cursos')->onDelete('cascade');
+            $table->unsignedBigInteger('id_profesor')->nullable();
+            $table->foreign('id_profesor')->references('id')->on('profesores')->onDelete('set null');
+            $table->unsignedBigInteger('id_horario')->nullable();
+            $table->foreign('id_horario')->references('id')->on('horarios')->onDelete('set null');
+            $table->string('semestre');
+            $table->date('fecha_matricula');
+            $table->decimal('nota_final', 5, 2)->nullable();
+            $table->enum('estado', ['aprobado', 'reprobado', 'cursando'])->default('cursando');
+            
             $table->timestamps();
         });
     }
